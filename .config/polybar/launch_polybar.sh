dc2f9f672c7aae4c9874a0db85a572e05ab071e9
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-10-10 23:48:55 +0530

    Add details to polybar

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+killall polybar
+
+xrdb ~/.Xresources
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload tanaybar &
   done
 else
   polybar --reload example &
