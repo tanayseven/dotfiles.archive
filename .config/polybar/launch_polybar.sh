007c19d93396b3619b18e8cb7dd3e283584eff8a
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-10-24 00:01:44 +0530

    Unclutter polybar by splitting it into two different bars

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -6,9 +6,11 @@ xrdb ~/.Xresources
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload tanaybar &
+    MONITOR=$m polybar --reload top-bar &
+    MONITOR=$m polybar --reload bottom-bar &
   done
 else
-  polybar --reload example &
+  polybar --reload top-bar &
+  polybar --reload bottom-bar &
 fi
 
